46526c4e244b7faf93d87cd991dcf3d48d25ee9c
Connor Fowler <connorfowlerroth@gmail.com>
2022-08-04 19:02:13 -0400

    DbContext added and program.cs set up

--- ElevenNote.WebAPI/Program.cs
+++ ElevenNote.WebAPI/Program.cs
@@ -1,6 +1,10 @@
+using ElevenNote.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +12,10 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
