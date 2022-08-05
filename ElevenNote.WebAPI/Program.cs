938799e5124937d5a84b5ab3bc5287889c79352b
Connor Fowler <connorfowlerroth@gmail.com>
2022-08-04 22:53:15 -0400

    UserService.cs and IUserService.cs added

--- ElevenNote.WebAPI/Program.cs
+++ ElevenNote.WebAPI/Program.cs
@@ -1,4 +1,5 @@
 using ElevenNote.Data;
+using ElevenNote.Services.User;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+// ADD User Service/Interface for Dependency Injection here
+builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
